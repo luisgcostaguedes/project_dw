0b80edccc8b12d7641c3c0022eff0eff90abf06e
luisgcostaguedes <luisgcostaguedes@gmail.com>
2022-07-21 12:38:37 -0300

    vai

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">CG Dev</a>
+      </header>
+      <main>Lista de produtos</main>
+    </div>
+  );
+}
+
+export default App;
