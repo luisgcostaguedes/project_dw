d1d9e60140e5a4714b4c0cf7386746976349a520
luisgcostaguedes <luisgcostaguedes@gmail.com>
2022-07-21 20:05:29 -0300

    up img

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # E-COMMERCE
 
 #Projeto desenvolvido na disciplina de Desenvolvimento Web da Universidade Federal de Itajubá
+
+# Passos para a criação deste projeto:
+
+1. Criação do diretório raiz
+2. Instalção das ferramentas necessárias (node)
+3. Inicialização de um projeto em React
+4. Criação de um repositório Git
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
+import data from './data';
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">CG Dev</a>
       </header>
-      <main>Lista de produtos</main>
+      <main>
+        <h1>Features Products</h1>
+
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to card</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
